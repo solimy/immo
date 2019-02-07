47238dd0be9d0f967eda52f413c11a8499c89500
hadrien <hadrien.daures@gmail.com>
2019-02-07 16:42:12 +0100

    added post parameters

--- test/test_api_immo_add.py
+++ test/test_api_immo_add.py
@@ -1,4 +1,5 @@
 import requests
-r = requests.post("http://localhost:5000/immo/add", json={"token":"thetoken"})
+#changer le nom, ajouter une pièce, etc…
+r = requests.post("http://localhost:5000/immo/add", json={"token":"thetoken", "title":"arandomtitle", "rooms":"666"})
 print(r.status_code)
 print(r.json())
--- test/test_api_user_edit.py
+++ test/test_api_user_edit.py
@@ -1,4 +1,4 @@
 import requests
-r = requests.post("http://localhost:5000/user/edit", json={"token":"thetoken"})
+r = requests.post("http://localhost:5000/user/edit", json={"token":"thetoken", "first_name":"arandomfirstname", "last_name":"arandomlastname", "birth_date":"yyyymmdd"})
 print(r.status_code)
 print(r.json())
