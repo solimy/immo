ef04357c5ab70832cd2964f0e8ddd0b6cf035fa3
Hadrien DAURES <hadrien.daures@gmail..com>
2019-02-08 02:40:41 +0100

    api immo list

--- api/immo/immo_list.py
+++ api/immo/immo_list.py
@@ -1,13 +1,32 @@
 from flask import Blueprint, request
 from ResponseObject import ResponseObject
 import decorators
+import api
+import db
+import sqlite3 as sql
 
 list = Blueprint('immo_list', __name__)
 
-@list.route("/immo/list", methods = ["GET", "POST"])
+@list.route("/immo/list", methods = ["POST"])
 @decorators.debug_request_display
+@decorators.required_param_token
 def handle():
     if request.method == "POST":
-        return ResponseObject(400, "list").json()
-    if request.method == "GET":
-        return ResponseObject(400, "list").json()
+        status = api.SUCCESS
+        response = None
+        try:
+            token = request.json[api.TOKEN]
+            with sql.connect(db.DB) as con:
+                cur = con.cursor()
+                cur.execute("SELECT "+ db.TITLE+" FROM "+db.IMMOS)
+                immos = cur.fetchall()
+                response = {api.TITLE:[*map(lambda immo: immo[0], immos)]}
+        except Exception as e:
+            print(e)
+            status = api.ERROR
+            response = None
+            con.rollback()
+
+        finally:
+            return ResponseObject(status, response).json()
+            con.close()
